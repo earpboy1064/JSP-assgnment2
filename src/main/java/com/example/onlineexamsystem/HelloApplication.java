2f0e324fba90a91ccab746b9bf88d134bd1d2cfc
earpboy1064 <31712696+earpboy1064@users.noreply.github.com>
2023-04-14 19:50:01 -0500

    FirstPushForProject1
    No real code has been added this is an empty project

---
+++ src/main/java/com/example/onlineexamsystem/HelloApplication.java
@@ -0,0 +1,9 @@
+package com.example.onlineexamsystem;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
